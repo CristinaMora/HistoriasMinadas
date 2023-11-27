5401c3a33b1486fb8eca0d9096cdecfb2749f631
Cristina Mora <Crmora03@ucm.es>
2023-11-27 10:25:14 +0100

    :)

--- juegosSerios/Assets/Scripts/CordobaScenenManager.cs
+++ juegosSerios/Assets/Scripts/CordobaScenenManager.cs
@@ -5,22 +5,44 @@ using UnityEngine.UI;
 
 public class CordobaScenenManager : MonoBehaviour
 {
+    public static CordobaScenenManager instance;
     #region references
     [SerializeField]
     private CordobaUI uiManager;
     [SerializeField]
     LevelData _levelData;
     #endregion
+    private void Awake()
+    {
+        // Verifica si ya existe una instancia del GameManager.
+        if (instance == null)
+        {
+            // Si no existe, establece esta instancia como la instancia única.
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            // Si ya existe una instancia, destruye esta para evitar duplicados.
+            Destroy(gameObject);
+        }
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-       Debug.Log(_levelData.texto[0]) ;
         
     }
-
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public string nextText(int num)
+    {
+        Debug.Log(num);
+        if (_levelData.texto.Length > num)
+            return _levelData.texto[num];
+        else return "fallo";
     }
 }
--- juegosSerios/Assets/Scripts/CordobaUI.cs
+++ juegosSerios/Assets/Scripts/CordobaUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,30 +11,43 @@ public class CordobaUI : MonoBehaviour
     [SerializeField] private GameObject _textoini;
     [SerializeField] private GameObject _fotopatios;
     [SerializeField] private GameObject _isabel;
+    [SerializeField] private GameObject _bocadillo;
+    [SerializeField] private TMP_Text _texto;
     [SerializeField] private Button _inicio;
+    int numdialog;
     // Start is called before the first frame update
     void Start()
     {
         _imagenini.SetActive(true);
         _textoini.SetActive(true);
         _isabel.SetActive(false);
+        _bocadillo.SetActive(false);
         _fotopatios.SetActive(false);
         _inicio.gameObject.SetActive(true);
+        _texto.gameObject.SetActive(false);
+        numdialog = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+               
     }
     public void patios()
     {
         _imagenini.SetActive(false);
         _textoini.SetActive(false);
         _isabel.SetActive(true);
+        _bocadillo.SetActive(true);
         _fotopatios.SetActive(true);
         _inicio.gameObject.SetActive(false);
-
+        _texto.gameObject.SetActive(true);
+        _texto.text = CordobaScenenManager.instance.nextText(numdialog); 
+    }
+    public void nextdialog()
+    {
+        numdialog++;
+        _texto.text = CordobaScenenManager.instance.nextText(numdialog);
 
     }
 }
