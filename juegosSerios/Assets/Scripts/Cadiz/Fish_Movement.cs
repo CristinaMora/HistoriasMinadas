a5a0b1a1bef259c2d1cc3cd07d909ebf7ed5389b
sheilajulvez <sjulvez@ucm.es>
2023-12-05 16:25:43 +0100

    ñ

---
+++ juegosSerios/Assets/Scripts/Cadiz/Fish_Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish_Movement : MonoBehaviour
+{
+    public Rigidbody2D _myrigidbody;
+    public Transform _mytransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) == true)
+        {
+           
+        }
+    }
+}
